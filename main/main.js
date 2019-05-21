bdd816c066ed249fdd7f994f02f89d07ba0f2760
rivalove <542763565@qq.com>
2019-05-21 22:53:19 +0800

    finish

--- main/main.js
+++ main/main.js
@@ -1,4 +1,14 @@
 module.exports = function main(inputs) {
-    // write your code here...
-    return "price";
+  // write your code here...
+  let {distance, parkTime} = inputs
+  let price = null
+  if (distance <= 2) {
+    price = 6
+  } else if (distance <= 8) {
+    price = 6 + 0.8 * (distance - 2)
+  } else {
+    price = 10.8 + (distance - 8) * 1.2
+  }
+  price += 0.25 * parkTime
+  return Math.round(price);
 };
